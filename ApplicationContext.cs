4bc427416db5ad3d42a66fae33ed6ca5530c98e9
Vi <asukca2@gmail.com>
2023-09-14 19:57:23 +0300

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Реестр_маневренного_фонда
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<ImprovementDegree> ImprovementDegree { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\DNS\\OneDrive\\Рабочий стол\\Visual Studio\\Реестр маневренного фонда\\database\\ManeuverFundRegister.db");
+        }
+    }
+}
