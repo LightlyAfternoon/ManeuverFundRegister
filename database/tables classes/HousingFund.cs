3a1c0fb06083e0f2703193c762465e40450ccd5d
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-09-21 10:16:39 +0300

    Update HousingFund.cs

--- database/tables classes/HousingFund.cs
+++ database/tables classes/HousingFund.cs
@@ -6,5 +6,29 @@ namespace Реестр_маневренного_фонда
     {
         [Key]
         public int IdHouse { get; set; }
+        public int StreetId { get; set; }
+        public int HouseNumber { get; set; }
+        public int? ApartmentNumber { get; set; }
+        public int? RoomNumber { get; set; }
+        public int ImprovementDegreeId { get; set; }
+        public float? DecreeArea { get; set; }
+        public float? RegisterArea { get; set; }
+        public string? Remark { get; set; }
+
+        public Street Street { get; set; }
+        public ImprovementDegree ImprovementDegree { get; set; }
+
+        public string getFullAddress() 
+        {
+            string LocalityName = "";
+            string StreetName = "";
+            
+            if (ApartmentNumber == null)
+                return $"{LocalityName}, ул. {StreetName}, д. {HouseNumber}"
+            else if (RoomNumber == null)
+                return $"{LocalityName}, ул. {StreetName}, д. {HouseNumber}, кв. {ApartmentNumber}"
+            else
+                return $"{LocalityName}, ул. {StreetName}, д. {HouseNumber}, кв. {ApartmentNumber}, ком. {RoomNumber}"
+        }
     }
 }
