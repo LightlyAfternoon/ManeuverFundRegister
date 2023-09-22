83ca7684eff1d29691c4d439cf346e55030dbb94
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-09-22 11:57:13 +0300

    Update ResidenceRegistration.cs

--- database/tables classes/ResidenceRegistration.cs
+++ database/tables classes/ResidenceRegistration.cs
@@ -6,5 +6,12 @@ namespace Реестр_маневренного_фонда
     {
         [Key]
         public int IdRegistration { get; set; }
+        public int HouseId { get; set; }
+        public int TempResidentId { get; set; }
+        public DateTime DateStartResidence { get; set; }
+        public DateTime? DateEndResidence { get; set; }
+
+        public TempResident TempResident { get; set; }
+        public HousingFund HousingFund { get; set; }
     }
 }
