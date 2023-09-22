2f843e692ec74190e2b2f31b569a2e9348a074a9
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-09-22 11:53:43 +0300

    Update Notification.cs

--- database/tables classes/Notification.cs
+++ database/tables classes/Notification.cs
@@ -6,5 +6,9 @@ namespace Реестр_маневренного_фонда
     {
         [Key]
         public int IdNotification { get; set; }
+        public int AgreementId { get; set; }
+        public DateTime RecievingDate { get; set; }
+
+        public Agreement Agreement { get; set; }
     }
 }
