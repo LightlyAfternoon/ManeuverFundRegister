b262e18b178cbb7ab5c219ca6d527e5aee904a87
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-09-22 12:02:02 +0300

    Update ApplicationContext.cs

--- database/ApplicationContext.cs
+++ database/ApplicationContext.cs
@@ -5,8 +5,13 @@ namespace Реестр_маневренного_фонда
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Agreement> Agreement { get; set; }
+        public DbSet<Decree> Decree { get; set; }
+        public DbSet<HousingFund> HousingFund { get; set; }
         public DbSet<ImprovementDegree> ImprovementDegree { get; set; }
         public DbSet<Locality> Locality { get; set; }
+        public DbSet<Notification> Notification { get; set; }
+        public DbSet<ResidenceRegistration> ResidenceRegistration { get; set; }
         public DbSet<Street> Street { get; set; }
         public DbSet<TempResident> TempResident { get; set; }
 
