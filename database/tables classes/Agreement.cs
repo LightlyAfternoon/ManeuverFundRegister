467d0ae342a7cf71cb95a97d0aa2b2f918f68bbe
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-09-25 09:11:59 +0300

    Update Agreement.cs

--- database/tables classes/Agreement.cs
+++ database/tables classes/Agreement.cs
@@ -8,6 +8,13 @@ namespace Реестр_маневренного_фонда
         public int IdAgreement { get; set; }
         public int NumberAgreement { get; set; }
         public int TempResidentId { get; set; }
+        public int HouseId { get; set; }
+        public DateTime DateConclusionAgreement { get; set; }
+        public DateTime DateEndAgreement { get; set; }
+        public DateTime? DateTerminationAgreement { get; set; }
+        public string Remark { get; set; }
 
+        public TempResident TempResident { get; set; }
+        public HousingFund HousingFund { get; set; }
     }
 }
