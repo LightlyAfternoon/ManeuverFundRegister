f72e6b2b0871fa6662114267fe0d191a73d0e22a
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-09-27 10:20:29 +0300

    upd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Реестр_маневренного_фонда.Pages;
 
 namespace Реестр_маневренного_фонда
@@ -32,6 +19,11 @@ namespace Реестр_маневренного_фонда
         {
             tb.Text = ApplicationContext.GetContext().TempResident.First(t => t.IdTempResident == 1).FullName;
             fr_Frame.Navigate(new AgreementsViewPage());
+        }
+
+        private void bt_DecreesViewPage_Click(object sender, RoutedEventArgs e)
+        {
+            fr_Frame.Navigate(new DecreesViewPage());
         }
     }
 }
