7707c4ede862183b1859fb0c960c831d0a6a350b
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-10-24 10:29:00 +0300

    Update LocalityManager.cs

--- TablesManagersClasses/LocalityManager.cs
+++ TablesManagersClasses/LocalityManager.cs
@@ -9,19 +9,91 @@ namespace Реестр_маневренного_фонда.TablesManagersClasses
 {
     public class LocalityManager
     {
-        public void AddLocality()
-        {
+        private ApplicationContext dbContext = ApplicationContext.GetContext();
 
+        private string errors = string.Empty;
+
+        private void showErrors(string? nameLocality)
+        {
+            if (string.IsNullOrWhiteSpace(nameLocality))
+            {
+                errors += ("Неоходимо ввести название населённого пункта");
+            }
+
+            MessageBox.Show(errors);
+            errors = string.Empty;
+        }
+     
+        public void AddLocality(string? nameLocality)
+        {
+            Locality newLocality = new Locality();
+            
+            showErrors(nameLocality);
+            if (!string.IsNullOrEmpty(errors))
+            {
+                MessageBox.Show(errors);
+                errors = string.Empty;
+            }
+            else
+            {
+                try
+                {
+                    newLocality.NameLocality = nameLocality;
+
+                    dbContext.Locality.Add(newLocality);
+                    dbContext.SaveChanges();
+
+                    MessageBox.Show("Населённый пункт добавлен", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MainFrameObj.mainFrame.Navigate(new LocalitiesViewPage());
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось добавить населённый пункт", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
-        public void EditLocality(Locality currentLocality)
+        public void EditLocality(Locality currentLocality, string? nameLocality)
         {
+            showErrors(nameLocality);
+            if (!string.IsNullOrEmpty(errors))
+            {
+                MessageBox.Show(errors);
+                errors = string.Empty;
+            }
+            else
+            {
+                try
+                {
+                    currentLocality.NameLocality = nameLocality;
 
+                    dbContext.Locality.Update(currentLocality);
+                    dbContext.SaveChanges();
+
+                    MessageBox.Show("Населённый пункт добавлен", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MainFrameObj.mainFrame.Navigate(new LocalitiesViewPage());
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось добавить населённый пункт", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
         public void RemoveLocality(Locality currenLocality)
         {
-
+            try
+            {
+                dbContext.Locality.Remove(currenLocality);
+                dbContext.SaveChanges();
+                
+                MessageBox.Show("Населённый пункт удалён", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                MainFrameObj.mainFrame.Navigate(new LocalitiesViewPage());
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось удалить населённый пункт", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
