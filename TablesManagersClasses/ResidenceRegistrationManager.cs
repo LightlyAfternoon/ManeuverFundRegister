6230db5b8f07064cbd38ae9d8a9c81d3a20f38d2
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-10-30 10:45:54 +0300

    Update ResidenceRegistrationManager.cs

--- TablesManagersClasses/ResidenceRegistrationManager.cs
+++ TablesManagersClasses/ResidenceRegistrationManager.cs
@@ -9,9 +9,48 @@ namespace Реестр_маневренного_фонда.TablesManagersClasses
 {
     public class ResidenceRegistrationManager
     {
-        public void AddResidenceRegistration()
+        private ApplicationContext dbContext = ApplicationContext.GetContext();
+        
+        public void AddResidenceRegistration(Agreement? newAgreement, string? errors)
         {
+            ResidenceRegistration lastRegistration = dbContext.ResidenceRegistration.Last(r => r.HousingFundId == newAgreement.HousingFundId);
+            
+            ResidenceRegistration newRegistration = new ResidenceRegistration();
+            if (dbContext.ResidenceRegistration.Count(r => r.HousingFundId == newAgreement.HousingFundId && lastRegistration.DateEndResidence == null) > 0)
+            {
+                if (lastRegistration.TempResidentId != newAgreement.TempResidentId)
+                {
+                    MessageBoxResult boxResult = MessageBox.Show($"Для данного жилья на данный момент проживающим числится {lastRegistration.TempResident.FullName}. Поставить дату окончания его проживания в жилье и назначить сейчас проживающим {newAgreement.TempResident.FullName}?", "", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
+                    switch (boxResult)
+                    {
+                        case MessageBoxResult.Yes:
+                            lastRegistration.DateStartResidence = (DateTime)dateConclusion;
+                                    
+                            newRegistration.HousingFundId = newAgreement.HousingFundId;
+                            newRegistration.TempResidentId = newAgreement.TempResidentId;
+                            newRegistration.DateStartResidence = newAgreement.dateConclusion;
+                            newRegistration.StartAgreementId = newAgreement.IdAgreement;
+                                    
+                            dbContext.ResidenceRegistration.Add(newRegistration);
+                            break;
+                        case MessageBoxResult.No:
+                            MessageBox.Show("Необходимо выбрать другое жильё", "", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                            errors += "Необходимо выбрать другое жильё";
+                            AddAgreement(newAgreement.number, newAgreement.TempResident, newAgreement.HousingFund, newAgreement.DateConclusion, newAgreement.DateEnd, newAgreement.Remark);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                newRegistration.HousingFundId = newAgreement.HousingFundId;
+                newRegistration.TempResidentId = newAgreement.TempResidentId;
+                newRegistration.DateStartResidence = newAgreement.DateConclusion;
+                newRegistration.StartAgreementId = newAgreement.IdAgreement;
+                        
+                dbContext.ResidenceRegistration.Add(newRegistration);
+            }
         }
 
         public void EditResidenceRegistration(ResidenceRegistration currentResidenceRegistration)
