2c1d622b0fb87c50b9ced2e7fdf5122455635bc1
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-10-30 11:26:19 +0300

    Update ResidenceRegistrationManager.cs

--- TablesManagersClasses/ResidenceRegistrationManager.cs
+++ TablesManagersClasses/ResidenceRegistrationManager.cs
@@ -10,6 +10,41 @@ namespace Реестр_маневренного_фонда.TablesManagersClasses
     public class ResidenceRegistrationManager
     {
         private ApplicationContext dbContext = ApplicationContext.GetContext();
+
+        private string errors = string.Empty;
+
+        private void showErrors(HousingFund? housingFund, TempResident? tempResident, DateTime? dateStartResidence, DateTime? dateEndResidence, Agreement? startAgreement)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+            {
+                errors += ("Неоходимо ввести номер\n");
+            }
+            else
+            {
+                try
+                {
+                    checked
+                    {
+                        Convert.ToInt32(number);
+                    }
+                }
+                catch
+                {
+                    errors += ("Укажите номер постановления корректно (только цифры)\n");
+                }
+            }
+            if (housingFund == null)
+            {
+                errors += ("Необходимо выбрать жильё\n");
+            }
+            if (dateDecree == null)
+            {
+                errors += ("Необходимо выбрать дату постановления");
+            }
+
+            MessageBox.Show(errors);
+            errors = string.Empty;
+        }
         
         public void AddResidenceRegistration(Agreement? newAgreement)
         {
@@ -56,12 +91,50 @@ namespace Реестр_маневренного_фонда.TablesManagersClasses
 
         public void EditResidenceRegistration(ResidenceRegistration currentResidenceRegistration)
         {
-            
+            showErrors(housingFund, tempResident, dateStartResidence, dateEndResidence, startAgreement);
+            if (!string.IsNullOrEmpty(errors))
+            {
+                MessageBox.Show(errors);
+                errors = string.Empty;
+            }
+            else
+            {
+                try
+                {
+                    currentResidenceRegistration.HousingFundId = housingFund.IdHousingFund;
+                    currentResidenceRegistration.TempResidentId = tempResident.IdTempResident;
+                    currentResidenceRegistration.DateStartResidence = dateStartResidence;
+                    currentResidenceRegistration.DateEndResidence = dateEndResidence;
+                    currentResidenceRegistration.StartAgreementId = startAgreement.IdStartAgreement;
+
+
+                    dbContext.ResidenceRegistration.Update(currentResidenceRegistration);
+                    dbContext.SaveChanges();
+
+                    MessageBox.Show("Факт проживания изменён", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MainFrameObj.mainFrame.Navigate(new ResidenceRegistrationViewPage());
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось изменить факт проживания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
-        public void RemoveResidenceRegistration(Locality currenResidenceRegistration)
+        public void RemoveResidenceRegistration(ResidenceRegistration currenResidenceRegistration)
         {
-
+            try
+            {
+                dbContext.ResidenceRegistration.Remove(currentResidenceRegistration);
+                dbContext.SaveChanges();
+                
+                MessageBox.Show("Факт проживания удалён", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                MainFrameObj.mainFrame.Navigate(new ResidenceRegistrationViewPage());
+            }
+            catch
+            {
+                MessageBox.Show("Не получилось удалить факт проживания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
