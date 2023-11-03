40572edfa5dff53cd9580b413c1f49b17d6a5e12
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-11-03 11:58:34 +0300

    add code for attach file

--- Pages/Agreements/AddNewAgreementPage.xaml.cs
+++ Pages/Agreements/AddNewAgreementPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 using Реестр_маневренного_фонда.database.tables_classes;
 using Реестр_маневренного_фонда.TablesManagersClasses;
 
@@ -23,6 +14,8 @@ namespace Реестр_маневренного_фонда.Pages.Agreements
     public partial class AddNewAgreementPage : Page
     {
         ApplicationContext dbContext = ApplicationContext.GetContext();
+
+        Agreement newAgreement = new();
 
         List<Decree> listDecrees = new List<Decree>();
         List<HousingFund> listAvailableHousingFund = new List<HousingFund>();
@@ -54,12 +47,18 @@ namespace Реестр_маневренного_фонда.Pages.Agreements
         private void bt_Add_Click(object sender, RoutedEventArgs e)
         {
             AgreementManager am = new AgreementManager();
-            am.AddAgreement(tb_Number.Text, cmb_TempReident.SelectedItem as TempResident, cmb_HousingFund.SelectedItem as HousingFund, dp_DateConclusion.SelectedDate, dp_DateEnd.SelectedDate, tb_Remark.Text);
+            am.AddAgreement(newAgreement,tb_Number.Text, cmb_TempReident.SelectedItem as TempResident, cmb_HousingFund.SelectedItem as HousingFund, dp_DateConclusion.SelectedDate, dp_DateEnd.SelectedDate, tb_Remark.Text);
         }
 
         private void bt_AttachFile_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new();
+            openFileDialog.Filter = "Word файлы (*.doc; *.docx)|*.doc;*.docx";
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                newAgreement.File = FileManager.attachFile(openFileDialog.FileName);
+            }
         }
     }
 }
