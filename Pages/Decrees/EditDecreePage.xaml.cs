592b1ee4a6f1f942730a8253678f982505301701
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-11-08 10:02:53 +0300

    upd

--- Pages/Decrees/EditDecreePage.xaml.cs
+++ Pages/Decrees/EditDecreePage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 using Реестр_маневренного_фонда.TablesManagersClasses;
 
 namespace Реестр_маневренного_фонда.Pages.Decrees
@@ -62,7 +52,13 @@ namespace Реестр_маневренного_фонда.Pages.Decrees
 
         private void bt_AttachFile_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new();
+            openFileDialog.Filter = "Word файлы (*.doc; *.docx)|*.doc;*.docx";
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                decree.File = FileManager.attachFile(openFileDialog.FileName);
+            }
         }
     }
 }
