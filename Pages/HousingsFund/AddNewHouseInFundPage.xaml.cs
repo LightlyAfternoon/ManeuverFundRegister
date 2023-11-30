1e91e1fd7c4f7a78f8bf064fcc2dc114279e73fc
LightlyAfternoon <136237924+LightlyAfternoon@users.noreply.github.com>
2023-11-30 10:55:58 +0300

    Update AddNewHouseInFundPage.xaml.cs

--- Pages/HousingsFund/AddNewHouseInFundPage.xaml.cs
+++ Pages/HousingsFund/AddNewHouseInFundPage.xaml.cs
@@ -42,7 +42,7 @@ namespace Реестр_маневренного_фонда.Pages.HousingsFund
             HousingFundManager hm = new HousingFundManager();
             hm.AddHouseInFund(cmb_Locality.SelectedItem as Locality, cmb_Street.SelectedItem as Street, tb_HouseNumber.Text, tb_EntranceNumber.Text, 
                 tb_FloorNumber.Text, tb_ApartmentNumber.Text, tb_RoomNumber.Text, tb_DecreeArea.Text, tb_RegisterArea.Text, tb_Remark.Text, 
-                chb_CWS.IsChecked, chb_HWS.IsChecked, chb_CG.IsChecked, chb_BG.IsChecked, chb_SH.IsChecked, chb_CH.IsChecked, chb_Electricity.IsChecked, chb_KeysAvalibility.IsChecked);
+                chb_CWS.IsChecked, chb_HWS.IsChecked, chb_CG.IsChecked, chb_BG.IsChecked, chb_SH.IsChecked, chb_CH.IsChecked, chb_Electricity.IsChecked, chb_KeysAvalibility.IsChecked, chb_Sewerage.IsChecked);
         }
 
         private void cmb_Locality_TextChanged(object sender, TextChangedEventArgs e)
