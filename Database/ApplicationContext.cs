25e61c58faca1a0a155bf4ccd1ca27df2fc3b8dc
LightlyAfternoon <asukca2@gmail.com>
2023-12-18 00:29:33 +0300

    add try catch blocks

--- Database/ApplicationContext.cs
+++ Database/ApplicationContext.cs
@@ -30,8 +30,14 @@ namespace Реестр_маневренного_фонда
         //переопределение метода OnConfiguring для установления параметров подключения к бд
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlite("Data Source="+ Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString()).ToString()).ToString()).ToString()) + "\\ManeuverFundRegister\\Database\\ManeuverFundRegister.db");
-            optionsBuilder.UseSqlite("Data Source=" + Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()) + "\\Database\\ManeuverFundRegister.db");
+            if (Directory.Exists(Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()) + "\\Database\\ManeuverFundRegister.db"))
+            {
+                optionsBuilder.UseSqlite("Data Source=" + Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()) + "\\Database\\ManeuverFundRegister.db");
+            }
+            else
+            {
+                optionsBuilder.UseSqlite("Data Source="+ Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString()).ToString()).ToString()).ToString()) + "\\ManeuverFundRegister\\Database\\ManeuverFundRegister.db");
+            }
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,95 +34,106 @@ namespace Реестр_маневренного_фонда
 
             MainFrameObj.mainFrame = fr_Frame;
 
-            using (TaskService ts = new TaskService())
+            try
             {
-                // Создание процесса консольной программы для проверки уведомлений
-                Process.Start(Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()) + "\\ExecuteNotificationManagerClass.exe");
+                using (TaskService ts = new TaskService())
+                {
+                    // Создание процесса консольной программы для проверки уведомлений
+                    Process.Start(Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()) + "\\ExecuteNotificationManagerClass.exe");
 
-                // Создание новой задачи и добавление её описания
-                TaskDefinition td = ts.NewTask();
-                td.RegistrationInfo.Description = "Добавление и удаление уведомлений";
+                    // Создание новой задачи и добавление её описания
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = "Добавление и удаление уведомлений";
 
-                // Создание триггера, который будет запускать задачу в 10 часов каждый день
-                td.Triggers.Add(new DailyTrigger { DaysInterval = 1 });
+                    // Создание триггера, который будет запускать задачу в 10 часов каждый день
+                    td.Triggers.Add(new DailyTrigger { DaysInterval = 1 });
 
-                // Определение команды, которую нужно запустить
-                td.Actions.Add(new ExecAction(Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()) + "\\ExecuteNotificationManagerClass.exe"));
+                    // Определение команды, которую нужно запустить
+                    td.Actions.Add(new ExecAction(Directory.GetParent(Assembly.GetExecutingAssembly().Location.ToString()) + "\\ExecuteNotificationManagerClass.exe"));
 
-                // Регистрация задачи в планировщике
-                ts.RootFolder.RegisterTaskDefinition(@"AddAndRemoveNotifications", td);
+                    // Регистрация задачи в планировщике
+                    ts.RootFolder.RegisterTaskDefinition(@"AddAndRemoveNotifications", td);
+                }
             }
+            catch { }
         }
 
         private void UpdateDB()
         {
-            FormattableString addHouseDecreeTableCommand = $"CREATE TABLE IF NOT EXISTS HouseDecree (IdHouseDecree INTEGER NOT NULL, DecreeId INTEGER NOT NULL, HousingFundId INTEGER NOT NULL, FOREIGN KEY(DecreeId) REFERENCES Decree(IdDecree), FOREIGN KEY(HousingFundId) REFERENCES HousingFund(IdHousingFund), PRIMARY KEY(IdHouseDecree AUTOINCREMENT));";
-            dbContext = ApplicationContext.GetContext();
-            dbContext.Database.ExecuteSql(addHouseDecreeTableCommand);
-
-            foreach (Decree decree in dbContext.Decree.ToList())
-            {
-                int firstIdDecree = dbContext.Decree.First(d => d.NumberDecree == decree.NumberDecree && d.DateDecree == decree.DateDecree).IdDecree;
-                if (dbContext.HouseDecree.Count(hd => hd.DecreeId == firstIdDecree && hd.HousingFundId == decree.HousingFundId) < 1)
-                {
-                    HouseDecree houseDecree = new HouseDecree();
-                    houseDecree.DecreeId = firstIdDecree;
-                    houseDecree.HousingFundId = decree.HousingFundId;
-                    dbContext.HouseDecree.Add(houseDecree);
-
-                    if (decree != dbContext.Decree.First(d => d.NumberDecree == decree.NumberDecree && d.DateDecree == decree.DateDecree))
-                    {
-                        dbContext.Decree.Remove(decree);
-                    }
-                    dbContext.SaveChanges();
-                }
-            }
-
-            FormattableString changeAgreementNumberCommand = $"PRAGMA writable_schema=ON;PRAGMA foreign_keys=OFF; CREATE TABLE AgreementTemp (IdAgreement INTEGER NOT NULL UNIQUE,NumberAgreement INTEGER,TempResidentId INTEGER NOT NULL,HousingFundId INTEGER NOT NULL,DateConclusionAgreement DATETIME NOT NULL,DateEndAgreement DATETIME NOT NULL,DateTerminationAgreement DATETIME,Remark NVARCHAR,File VARBINARY,PRIMARY KEY(IdAgreement AUTOINCREMENT),FOREIGN KEY(HousingFundId) REFERENCES HousingFund(IdHousingFund),FOREIGN KEY(TempResidentId) REFERENCES TempResident(IdTempResident)); INSERT INTO AgreementTemp(IdAgreement,NumberAgreement,TempResidentId,HousingFundId,DateConclusionAgreement,DateEndAgreement,DateTerminationAgreement,Remark,File) SELECT IdAgreement,NumberAgreement,TempResidentId,HousingFundId,DateConclusionAgreement,DateEndAgreement,DateTerminationAgreement,Remark,File FROM Agreement; DROP TABLE Agreement; ALTER TABLE AgreementTemp RENAME TO Agreement; PRAGMA foreign_keys=ON; PRAGMA writable_schema=OFF;";
-            dbContext.Database.ExecuteSql(changeAgreementNumberCommand);
-
             try
             {
-                if (dbContext.HousingFund.Count(h => h.EntranceNumber != null) > 1)
+                FormattableString addHouseDecreeTableCommand = $"CREATE TABLE IF NOT EXISTS HouseDecree (IdHouseDecree INTEGER NOT NULL, DecreeId INTEGER NOT NULL, HousingFundId INTEGER NOT NULL, FOREIGN KEY(DecreeId) REFERENCES Decree(IdDecree), FOREIGN KEY(HousingFundId) REFERENCES HousingFund(IdHousingFund), PRIMARY KEY(IdHouseDecree AUTOINCREMENT));";
+                dbContext = ApplicationContext.GetContext();
+                dbContext.Database.ExecuteSql(addHouseDecreeTableCommand);
+
+                foreach (Decree decree in dbContext.Decree.ToList())
                 {
+                    int firstIdDecree = dbContext.Decree.First(d => d.NumberDecree == decree.NumberDecree && d.DateDecree == decree.DateDecree).IdDecree;
+                    if (dbContext.HouseDecree.Count(hd => hd.DecreeId == firstIdDecree && hd.HousingFundId == decree.HousingFundId) < 1)
+                    {
+                        HouseDecree houseDecree = new HouseDecree();
+                        houseDecree.DecreeId = firstIdDecree;
+                        houseDecree.HousingFundId = decree.HousingFundId;
+                        dbContext.HouseDecree.Add(houseDecree);
+
+                        if (decree != dbContext.Decree.First(d => d.NumberDecree == decree.NumberDecree && d.DateDecree == decree.DateDecree))
+                        {
+                            dbContext.Decree.Remove(decree);
+                        }
+                        dbContext.SaveChanges();
+                    }
                 }
 
+                FormattableString changeAgreementNumberCommand = $"PRAGMA writable_schema=ON;PRAGMA foreign_keys=OFF; CREATE TABLE AgreementTemp (IdAgreement INTEGER NOT NULL UNIQUE,NumberAgreement INTEGER,TempResidentId INTEGER NOT NULL,HousingFundId INTEGER NOT NULL,DateConclusionAgreement DATETIME NOT NULL,DateEndAgreement DATETIME NOT NULL,DateTerminationAgreement DATETIME,Remark NVARCHAR,File VARBINARY,PRIMARY KEY(IdAgreement AUTOINCREMENT),FOREIGN KEY(HousingFundId) REFERENCES HousingFund(IdHousingFund),FOREIGN KEY(TempResidentId) REFERENCES TempResident(IdTempResident)); INSERT INTO AgreementTemp(IdAgreement,NumberAgreement,TempResidentId,HousingFundId,DateConclusionAgreement,DateEndAgreement,DateTerminationAgreement,Remark,File) SELECT IdAgreement,NumberAgreement,TempResidentId,HousingFundId,DateConclusionAgreement,DateEndAgreement,DateTerminationAgreement,Remark,File FROM Agreement; DROP TABLE Agreement; ALTER TABLE AgreementTemp RENAME TO Agreement; PRAGMA foreign_keys=ON; PRAGMA writable_schema=OFF;";
+                dbContext.Database.ExecuteSql(changeAgreementNumberCommand);
+
+                try
+                {
+                    if (dbContext.HousingFund.Count(h => h.EntranceNumber != null) > 1)
+                    {
+                    }
+
+                }
+                catch
+                {
+                    FormattableString changeHousingFundCommand1 = $"ALTER TABLE HousingFund ADD COLUMN LocalityId INTEGER;ALTER TABLE HousingFund ADD COLUMN FloorNumber INTEGER;ALTER TABLE HousingFund ADD COLUMN CWS BIT;ALTER TABLE HousingFund ADD COLUMN HWS BIT;ALTER TABLE HousingFund ADD COLUMN CG BIT;ALTER TABLE HousingFund ADD COLUMN BG BIT;ALTER TABLE HousingFund ADD COLUMN SH BIT;ALTER TABLE HousingFund ADD COLUMN CH BIT;ALTER TABLE HousingFund ADD COLUMN Electricity BIT;ALTER TABLE HousingFund ADD COLUMN KeysAvailability BIT;ALTER TABLE HousingFund ADD COLUMN EntranceNumber INTEGER;";
+                    dbContext.Database.ExecuteSql(changeHousingFundCommand1);
+                    FormattableString changeHousingFundCommand2 = $"PRAGMA writable_schema=ON;PRAGMA foreign_keys=OFF;CREATE TABLE HousingFundTemp(IdHousingFund INTEGER NOT NULL UNIQUE,LocalityId INTEGER,StreetId INTEGER,HouseNumber NVARCHAR(10),EntranceNumber INTEGER,FloorNumber INTEGER,ApartmentNumber INTEGER,RoomNumber NVARCHAR(20),DecreeArea REAL,RegisterArea REAL,Remark NVARCHAR,CWS BIT,HWS BIT,CG BIT,BG BIT,SH BIT,CH BIT,Electricity BIT,KeysAvailability BIT,ImprovementDegreeId INTEGER,PRIMARY KEY(IdHousingFund AUTOINCREMENT),FOREIGN KEY(StreetId) REFERENCES Street(IdStreet),FOREIGN KEY(LocalityId) REFERENCES Locality(IdLocality),FOREIGN KEY(ImprovementDegreeId) REFERENCES ImprovementDegree(IdImprovementDegree));INSERT INTO HousingFundTemp(IdHousingFund,LocalityId,StreetId,HouseNumber,ApartmentNumber,RoomNumber,DecreeArea,RegisterArea,Remark,EntranceNumber,FloorNumber,CWS,HWS,CG,BG,SH,CH,Electricity,KeysAvailability,ImprovementDegreeId) SELECT h.IdHousingFund,s.LocalityId,s.IdStreet,h.HouseNumber,h.ApartmentNumber,h.RoomNumber,h.DecreeArea,h.RegisterArea,h.Remark,h.EntranceNumber,h.FloorNumber,h.CWS,h.HWS,h.CG,h.BG,h.SH,h.CH,h.Electricity,h.KeysAvailability,h.ImprovementDegreeId FROM HousingFund h INNER JOIN Street s ON h.StreetId=s.IdStreet; DROP TABLE HousingFund; ALTER TABLE HousingFundTemp RENAME TO HousingFund; PRAGMA foreign_keys=ON; PRAGMA writable_schema=OFF;";
+                    dbContext.Database.ExecuteSql(changeHousingFundCommand2);
+                    foreach (HousingFund housingFund in dbContext.HousingFund.ToList())
+                    {
+                        if (housingFund.CWS == null)
+                            housingFund.CWS = false;
+
+                        if (housingFund.HWS == null)
+                            housingFund.HWS = false;
+
+                        if (housingFund.BG == null)
+                            housingFund.BG = false;
+
+                        if (housingFund.CG == null)
+                            housingFund.CG = false;
+
+                        if (housingFund.CH == null)
+                            housingFund.CH = false;
+
+                        if (housingFund.SH == null)
+                            housingFund.SH = false;
+
+                        if (housingFund.Electricity == null)
+                            housingFund.Electricity = false;
+
+                        if (housingFund.KeysAvailability == null)
+                            housingFund.KeysAvailability = true;
+
+                        dbContext.HousingFund.Update(housingFund);
+                        dbContext.SaveChanges();
+                    }
+                }
             }
             catch
             {
-                FormattableString changeHousingFundCommand1 = $"ALTER TABLE HousingFund ADD COLUMN LocalityId INTEGER;ALTER TABLE HousingFund ADD COLUMN FloorNumber INTEGER;ALTER TABLE HousingFund ADD COLUMN CWS BIT;ALTER TABLE HousingFund ADD COLUMN HWS BIT;ALTER TABLE HousingFund ADD COLUMN CG BIT;ALTER TABLE HousingFund ADD COLUMN BG BIT;ALTER TABLE HousingFund ADD COLUMN SH BIT;ALTER TABLE HousingFund ADD COLUMN CH BIT;ALTER TABLE HousingFund ADD COLUMN Electricity BIT;ALTER TABLE HousingFund ADD COLUMN KeysAvailability BIT;ALTER TABLE HousingFund ADD COLUMN EntranceNumber INTEGER;";
-                dbContext.Database.ExecuteSql(changeHousingFundCommand1);
-                FormattableString changeHousingFundCommand2 = $"PRAGMA writable_schema=ON;PRAGMA foreign_keys=OFF;CREATE TABLE HousingFundTemp(IdHousingFund INTEGER NOT NULL UNIQUE,LocalityId INTEGER,StreetId INTEGER,HouseNumber NVARCHAR(10),EntranceNumber INTEGER,FloorNumber INTEGER,ApartmentNumber INTEGER,RoomNumber NVARCHAR(20),DecreeArea REAL,RegisterArea REAL,Remark NVARCHAR,CWS BIT,HWS BIT,CG BIT,BG BIT,SH BIT,CH BIT,Electricity BIT,KeysAvailability BIT,ImprovementDegreeId INTEGER,PRIMARY KEY(IdHousingFund AUTOINCREMENT),FOREIGN KEY(StreetId) REFERENCES Street(IdStreet),FOREIGN KEY(LocalityId) REFERENCES Locality(IdLocality),FOREIGN KEY(ImprovementDegreeId) REFERENCES ImprovementDegree(IdImprovementDegree));INSERT INTO HousingFundTemp(IdHousingFund,LocalityId,StreetId,HouseNumber,ApartmentNumber,RoomNumber,DecreeArea,RegisterArea,Remark,EntranceNumber,FloorNumber,CWS,HWS,CG,BG,SH,CH,Electricity,KeysAvailability,ImprovementDegreeId) SELECT h.IdHousingFund,s.LocalityId,s.IdStreet,h.HouseNumber,h.ApartmentNumber,h.RoomNumber,h.DecreeArea,h.RegisterArea,h.Remark,h.EntranceNumber,h.FloorNumber,h.CWS,h.HWS,h.CG,h.BG,h.SH,h.CH,h.Electricity,h.KeysAvailability,h.ImprovementDegreeId FROM HousingFund h INNER JOIN Street s ON h.StreetId=s.IdStreet; DROP TABLE HousingFund; ALTER TABLE HousingFundTemp RENAME TO HousingFund; PRAGMA foreign_keys=ON; PRAGMA writable_schema=OFF;";
-                dbContext.Database.ExecuteSql(changeHousingFundCommand2);
-                foreach (HousingFund housingFund in dbContext.HousingFund.ToList())
-                {
-                    if (housingFund.CWS == null)
-                        housingFund.CWS = false;
-
-                    if (housingFund.HWS == null)
-                        housingFund.HWS = false;
-
-                    if (housingFund.BG == null)
-                        housingFund.BG = false;
-
-                    if (housingFund.CG == null)
-                        housingFund.CG = false;
-
-                    if (housingFund.CH == null)
-                        housingFund.CH = false;
-
-                    if (housingFund.SH == null)
-                        housingFund.SH = false;
-
-                    if (housingFund.Electricity == null)
-                        housingFund.Electricity = false;
-
-                    if (housingFund.KeysAvailability == null)
-                        housingFund.KeysAvailability = true;
-
-                    dbContext.HousingFund.Update(housingFund);
-                    dbContext.SaveChanges();
-                }
+                MessageBox.Show("Не удалось изменить базу данных", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
